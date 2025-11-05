e9d63589504b95a2823c00b8e8f63b8694ff1887
elisa <elisa1.pdx@gmail.com>
2025-11-05 12:28:34 +0100

    collision done

--- front/src/game/Grid.js
+++ front/src/game/Grid.js
@@ -1,6 +1,5 @@
-import GeneratePiece from "./GeneratePiece";
 import { useEffect, useState } from "react";
-import GenerateGrid from "./GenerateGrid";
+import { showToast } from "../Toasts";
 
 const REFRESH = 1000;
 
@@ -13,20 +12,20 @@ function Grid() {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
       ],
-      color: "cyan",
+      color: "I",
     },
     O: {
       shape: [
         [1, 1],
         [1, 1],
       ],
-      color: "yellow",
+      color: "O",
     },
     J: {
       shape: [
-        [1, 0, 0],
-        [1, 1, 1],
-        [0, 0, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],
       ],
       color: "J",
     },
@@ -36,7 +35,7 @@ function Grid() {
         [1, 1, 1],
         [0, 0, 0],
       ],
-      color: "purple",
+      color: "T",
     },
     L: {
       shape: [
@@ -52,7 +51,7 @@ function Grid() {
         [1, 1, 0],
         [0, 0, 0],
       ],
-      color: "green",
+      color: "S",
     },
     Z: {
       shape: [
@@ -60,9 +59,10 @@ function Grid() {
         [0, 1, 1],
         [0, 0, 0],
       ],
-      color: "red",
+      color: "Z",
     },
   };
+
   const [grid, setGrid] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -86,8 +86,28 @@ function Grid() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
+
   const bag = ["J", "L", "O", "T", "S", "Z", "I"];
-  const playerBag = [];
+  const [playerBag, setPlayerBag] = useState([]);
+  const [start, setStart] = useState(false);
+  const [end, setEnd] = useState(false);
+  
+  function rotateMatrix90() {
+    const n = currentPiece.shape.length;
+    let rotated = Array.from({ length: n }, () => Array(n).fill(0));
+  
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        rotated[j][n - 1 - i] = currentPiece.shape[i][j];
+      }
+    }
+
+    if (!collaps(0, 0, rotated)) {
+      undraw()
+      currentPiece.shape = rotated;
+      draw()
+    }
+  }
 
   function getNewBag() {
     const newArray = Array.from(bag);
@@ -95,38 +115,84 @@ function Grid() {
       const j = Math.floor(Math.random() * (i + 1));
       [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
     }
-    console.log(newArray);
     return newArray;
   }
 
   function newPiece() {
+    let newP = playerBag ? playerBag.shift() : null;
     if (playerBag.length <= 2) {
-      playerBag.push(...getNewBag());
+      const newBag = [...playerBag, ...getNewBag()]
+      if (!newP) {
+        newP = newBag.shift()
+      }
+      setPlayerBag(prevState => [...newBag])
     }
     setCurrentPiece(prevState => {
       return {
-        ...pieces[playerBag.shift()],
+        ...pieces[newP],
         x: 0,
-        y: 0
+        y: 3
       }
     })
   }
 
   const [currentPiece, setCurrentPiece] = useState(null);
 
-  const update = () => {
-    undraw();
-    if (currentPiece !== null) {
-      currentPiece.x = currentPiece.x + 1;
+  const movePiece = (x, y) => {
+    if (currentPiece) {
+      if (!collaps(x, y)) {
+        undraw()
+        currentPiece.x = currentPiece.x + x;
+        currentPiece.y = currentPiece.y + y
+        draw()
+        return true
+      }
     }
-    draw();
+    return false
+  }
+
+  const collaps = (nx, ny, nshape = null) => {
+    const shape = nshape ? nshape : currentPiece.shape;
+    for (let i = 0; i < shape.length; i++) {
+      for (let j = 0; j < shape[i].length; j++) {
+        if (shape[i][j] === 0) {
+          continue
+        } else {
+          const cx = currentPiece.x + i + nx
+          const cy = currentPiece.y + j + ny
+          // bas && droite && gauche
+          if (cx >= grid.length || cy >= grid[0].length || cy < 0) {
+            return true
+          }
+
+          if (i + nx < shape.length && j + ny < shape[i].length) {
+            if (shape[i + nx][j + ny] === 1) continue
+          }
+          if (grid[cx][cy] !== 0) {
+            return true
+          }
+        }
+      }
+    }
+    return false
+  }
+
+  const update = () => {
+    if (start && currentPiece !== null) {
+      if (!movePiece(1, 0)) {
+        if (grid[0].some((v) => v !== 0)) {
+          showToast("error", "GAME OVER");
+          setStart(false)
+        } else newPiece()
+      }
+    }
   };
 
   const undraw = () => {
     currentPiece?.shape.map((row, i) => {
       row.map((col, j) => {
         if (col !== 0) {
-          grid[currentPiece.x + i][currentPiece.y + j] = "E";
+          grid[currentPiece.x + i][currentPiece.y + j] = 0;
         }
       });
     });
@@ -134,7 +200,6 @@ function Grid() {
   };
 
   const draw = () => {
-    console.log(currentPiece);
     currentPiece?.shape.map((row, i) => {
       row.map((col, j) => {
         if (col !== 0) {
@@ -145,20 +210,39 @@ function Grid() {
     setGrid([...grid]);
   };
 
+  const keydown = (e) => {
+    if (e.code === 'Space') {
+      setStart(true)
+    }
+    else if (e.code === 'ArrowRight') {
+      movePiece(0, 1)
+    }
+    else if (e.code === 'ArrowLeft') {
+      movePiece(0, -1)
+    }
+    else if (e.code === 'ArrowDown') {
+      movePiece(1, 0)
+    }
+    else if (e.code === 'ArrowUp') {
+      rotateMatrix90()
+    }
+  }
+
   useEffect(() => {
+    document.addEventListener("keydown", keydown)
+    draw();
     const iter = setInterval(() => {
       update();
     }, REFRESH);
-    return () => clearInterval(iter);
-  }, [currentPiece]);
+    return () => {
+      clearInterval(iter);
+      document.removeEventListener("keydown", keydown)
+    }
+  }, [currentPiece, start, playerBag]);
 
   useEffect(() => {
-    newPiece();
+    if (!currentPiece) newPiece();
   }, []);
-
-  useEffect(() => {
-    draw();
-  }, [currentPiece])
 
   return (
     <>
